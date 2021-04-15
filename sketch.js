d6e9d91938246cdd2a3a0ee49719f43f6ccf8abe
nickimirani <moni20nd@student.ju.se>
2021-04-15 18:33:03 +0200

    Update sketch.js
    I did the snakes and steve and gave them random positions, i will change their positions later

--- sketch.js
+++ sketch.js
@@ -6,4 +6,42 @@ function setup(){
 }
 
 function draw(){
+    //snake positions
+    snake1(40,40); 
+    snake2(100,40);
+    snake3(160, 40);
+    snake4(220, 40);
+    
+    //steve position
+    steve(200,100);
+
 }
+
+//player
+function steve(x,y){
+    fill(0, 10, 200);
+    rect(x,y, 30,30,10);
+}
+
+//snake1
+function snake1(x,y){
+    fill(10 , 100 , 30);
+    rect(x,y,50, 20);
+}
+//snake2
+function snake2(x,y){
+    fill(10 , 100 , 30);
+    rect(x,y,50, 20);
+}
+//snake3
+function snake3(x,y){
+    fill(10 , 100 , 30);
+    rect(x,y,50, 20);
+}
+//snake4
+function snake4(x,y){
+    fill(10 , 100 , 30);
+    rect(x,y,50, 20);
+}
+
+
